5aac6a71f67dcfe10c2c1a501e4cafd86e0e5bea
TheHuntedHunter <129537927+TheHuntedHunter@users.noreply.github.com>
2023-03-31 22:07:58 +0530

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,17 @@
+class data_preprocessing:
+
+    def normalize(x):
+        min_val = np.min(x, axis=0)
+        max_val = np.max(x, axis=0)
+        ranges = max_val - min_val 
+
+        normalized_data = (x - min_val) / ranges
+        return normalized_data
+
+    def standardize(x):
+        x_mean = np.mean(x, axis=0)
+        x_std = np.std(x, axis=0)
+        
+        standardized_data = (x - x_mean) / x_std
+        return standardized_data
+
